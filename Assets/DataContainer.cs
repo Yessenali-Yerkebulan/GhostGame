687a68ff2f97d16554ffb51375f78e439427359d
Yessenali Yerkebulan <yerkebulanyessenali03@gmail.com>
2025-03-05 06:15:32 +0500

    add death UI

---
+++ Assets/DataContainer.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class DataContainer
+{
+    public static int _coins = 0;
+    public static int _deaths = 0;
+}
---
+++ Assets/UIController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UIControl : MonoBehaviour
+{
+    [SerializeField] private TMP_Text coins;
+    [SerializeField] private TMP_Text deaths;
+    void Start()
+    {
+        coins.text = DataContainer._coins.ToString();
+        deaths.text = DataContainer._deaths.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
